4495eb0706d55bf320d38f1225471e8dc26832a1
CigdemKayaa <49146485+CigdemKayaa@users.noreply.github.com>
2019-04-30 01:18:58 +0300

    Initial commit

---
+++ TcKimlik/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TcKimlik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTc = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOnay = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC Kimlik";
+            // 
+            // txtTc
+            // 
+            this.txtTc.Location = new System.Drawing.Point(149, 55);
+            this.txtTc.Name = "txtTc";
+            this.txtTc.Size = new System.Drawing.Size(260, 20);
+            this.txtTc.TabIndex = 1;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(149, 109);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(260, 20);
+            this.txtAd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ad";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(149, 167);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(260, 20);
+            this.txtSoyad.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Soyad";
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(149, 230);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(118, 20);
+            this.txtYil.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dogum Yıl";
+            // 
+            // btnOnay
+            // 
+            this.btnOnay.Location = new System.Drawing.Point(334, 270);
+            this.btnOnay.Name = "btnOnay";
+            this.btnOnay.Size = new System.Drawing.Size(75, 23);
+            this.btnOnay.TabIndex = 8;
+            this.btnOnay.Text = "Onay/Kayıt";
+            this.btnOnay.UseVisualStyleBackColor = true;
+            this.btnOnay.Click += new System.EventHandler(this.btnOnay_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(240, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Kimlik Doğrulama";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 389);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnOnay);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTc);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTc;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnOnay;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ TcKimlik/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TcKimlik.ServiceKimlik;
+
+namespace TcKimlik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOnay_Click(object sender, EventArgs e)
+        {
+            KPSPublicSoapClient servis = new KPSPublicSoapClient();
+           
+            bool deger =  servis.TCKimlikNoDogrula(Convert.ToInt64(txtTc.Text), txtAd.Text.ToUpper(), txtSoyad.Text.ToUpper(), Convert.ToInt32(txtYil.Text));
+            if (deger==true)
+            {
+                MessageBox.Show("Dogru Kimlik");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış Kimlik");
+            }
+        }
+    }
+}
